5ec7f610bcb460ec9bc51da41599110082669c44
Ivan Nasanovich <37714995+Uolary@users.noreply.github.com>
2020-02-08 23:45:27 +0300

    added icons for error and loading weather info

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react';
 import Search from './Search';
 import WeatherInfo from './WeatherInfo';
 
-function App() {
-  return (
-    <div className="App">
-      <Search />
-      <WeatherInfo />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      weatherInfo: [],
+      hasErrored: false,
+      isLoading: false
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Search />
+        
+        {
+          this.state.hasErrored ? <WeatherInfo info={'Ошибка загрузки'} /> :
+          this.state.isLoading ? <WeatherInfo info={'Загрузка...'} /> : null
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 class WeatherInfo extends React.Component {
   render() {
+    let progressInfo = null
+
+    if (this.props.info === 'Ошибка загрузки') {
+      progressInfo = ( <i className="material-icons medium">error_outline</i> )
+    } else if (this.props.info === 'Загрузка...') {
+      progressInfo = ( <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div> )
+    }
+
     return (
       <div className="weather-info">
         <div className="container">
           <div className="row">
             <div className="col s12 m6 weather-city">
-              <div className="card blue-grey darken-1">
+              <div className="card teal lighten-1">
                 <div className="card-content white-text">
-                  <span className="card-title center-align">Погода в </span>
+                  {progressInfo}
+                  <span className="card-title center-align">{this.props.info}</span>
                 </div>
               </div>
             </div>
